c2207a3141d52180430b2e7381bf03c07417a72c
Gorky Muñoz <gorkymunoz@hotmail.com>
2019-10-07 17:53:26 -0500

    validacion matricula

--- PicoPlacaApp/app/src/main/java/com/gorkymunoz/picoplaca/ConsultarFragment.java
+++ PicoPlacaApp/app/src/main/java/com/gorkymunoz/picoplaca/ConsultarFragment.java
@@ -11,9 +11,15 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Calendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 /**
@@ -21,7 +27,7 @@ import com.google.android.material.textfield.TextInputLayout;
  */
 public class ConsultarFragment extends Fragment {
 
-    TextInputLayout layoutMatricula;
+    private TextInputLayout layoutMatricula;
 
 
     public ConsultarFragment() {
@@ -35,7 +41,16 @@ public class ConsultarFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_consultar, container, false);
         layoutMatricula = view.findViewById(R.id.layout_placa_ingresada);
+        final TextView matriculaIngresada = view.findViewById(R.id.placa_ingresada);
         FloatingActionButton fabHistorial = view.findViewById(R.id.fab_historial);
+        Button botonConsultar = view.findViewById(R.id.material_unelevated_button);
+        botonConsultar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                consultarPicoPlaca(matriculaIngresada.getText().toString(),calendar);
+            }
+        });
         fabHistorial.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,6 +58,13 @@ public class ConsultarFragment extends Fragment {
             }
         });
         return view;
+    }
+
+    private void consultarPicoPlaca(String matricula, Calendar calendar) {
+        if(validarMatricula(matricula)){
+            return;
+        }
+        calendar.getTime();
     }
 
     private void navegarHistorial(View view){
@@ -55,7 +77,21 @@ public class ConsultarFragment extends Fragment {
             layoutMatricula.setError(getResources().getString(R.string.obligatorio));
             valido = false;
         }
+        else if(!verificarFormato(matricula)){
+            layoutMatricula.setError(getResources().getString(R.string.formato_incorrecto));
+            valido = false;
+        }
+        else {
+            layoutMatricula.setError(null);
+        }
         return valido;
     }
+
+    private boolean verificarFormato(String matriculaAValidar){
+        Pattern patron = Pattern.compile("[a-zA-Z]{2}[a-zA-Z]?\\d{3}\\d?");
+        Matcher encaja = patron.matcher(matriculaAValidar);
+        return encaja.matches();
+    }
+
 
 }
