f8352cd8d82c7d5d4bcce0035bcc84f24da7c57c
Gorky Muñoz <gorkymunoz@hotmail.com>
2019-10-07 17:12:09 -0500

    Validar que no esté vacío

--- PicoPlacaApp/app/src/main/java/com/gorkymunoz/picoplaca/ConsultarFragment.java
+++ PicoPlacaApp/app/src/main/java/com/gorkymunoz/picoplaca/ConsultarFragment.java
@@ -1,22 +1,27 @@
 package com.gorkymunoz.picoplaca;
 
 
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputLayout;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ConsultarFragment extends Fragment {
+
+    TextInputLayout layoutMatricula;
 
 
     public ConsultarFragment() {
@@ -29,6 +34,7 @@ public class ConsultarFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_consultar, container, false);
+        layoutMatricula = view.findViewById(R.id.layout_placa_ingresada);
         FloatingActionButton fabHistorial = view.findViewById(R.id.fab_historial);
         fabHistorial.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +47,15 @@ public class ConsultarFragment extends Fragment {
 
     private void navegarHistorial(View view){
         Navigation.findNavController(view).navigate(R.id.action_consultarFragment_to_historialFragment);
+    }
+
+    private Boolean validarMatricula(String matricula){
+        boolean valido = true;
+        if(TextUtils.isEmpty(matricula)){
+            layoutMatricula.setError(getResources().getString(R.string.obligatorio));
+            valido = false;
+        }
+        return valido;
     }
 
 }
